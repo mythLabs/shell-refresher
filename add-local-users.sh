bdb4ceb3e83c3df370db14fddffa6e52b608eba3
mythLabs <amitbisht744@gmail.com>
2021-11-25 16:54:39 +0530

    Update add-local-users with validations

--- add-local-users.sh
+++ add-local-users.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ "${UID}" -ne 0 ]]
+then
+  echo "Script can only be executed with root privileges"
+  exit 1
+fi
+
 # create user on local system
 read -p 'Enter username to create:' USER_NAME
 
@@ -11,6 +17,16 @@ read -p 'Enter password:' PASSWORD
 
 useradd -c "${COMMENT}" -m ${USER_NAME}
 
+if [[ "${?}" -ne 0 ]]
+then
+ echo "Error in creating the account"
+ exit 1
+fi
+
 echo ${PASSWORD} | passwd --stdin ${USER_NAME}
 
 passwd -e ${USER_NAME}
+
+echo "username: ${USER_NAME}"
+echo "password: ${PASSWORD}"
+echo "host: ${HOSTNAME}"
